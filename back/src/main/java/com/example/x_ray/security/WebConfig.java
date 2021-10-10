dca27f7ac7adaac8989459898480abd76c2baf5a
alska95 <rhsalska55@naver.com>
2021-10-10 14:59:24 +0900

    Back-end : cors문제 해결함
    done : addCorsMapping을 해줬어야 했었음.
    
    todo : Front-end 개발하며 필요한거 그때 그때 수정

--- back/src/main/java/com/example/x_ray/security/WebConfig.java
+++ back/src/main/java/com/example/x_ray/security/WebConfig.java
@@ -1,11 +1,20 @@
 package com.example.x_ray.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInCheckInterceptor())
